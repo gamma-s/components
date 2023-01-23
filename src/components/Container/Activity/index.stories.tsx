fe19cc157529e9ab0d0db4761e504db26b83e1dc
Caner Akdas <canerakdas@gmail.com>
2023-01-23 23:43:28 +0300

    feat: activity container storybook

---
+++ src/components/Container/Activity/index.stories.tsx
@@ -0,0 +1,70 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import AppButton from '../../Button/App';
+import TabButton from '../../Button/Tab';
+
+import {Apps, Tabs, Activity} from './';
+
+export default [
+  {
+    title: 'Container/Apps',
+    component: Apps,
+    args: {},
+  } as ComponentMeta<typeof Apps>,
+  {
+    title: 'Container/Tabs',
+    component: Tabs,
+    args: {},
+  } as ComponentMeta<typeof Tabs>,
+  {
+    title: 'Container/Activity',
+    component: Activity,
+    args: {},
+  } as ComponentMeta<typeof Activity>,
+];
+
+export const AppsContainer: ComponentStory<typeof Apps> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <Apps {...args}>
+      <AppButton
+        buttonDescription="This is a button"
+        buttonImage="https://www.google.com/images/branding/googleg/1x/googleg_standard_color_128dp.png"
+      />
+      <AppButton
+        buttonDescription="This is a another button"
+        buttonImage="https://www.youtube.com/s/desktop/82a4cf4f/img/favicon_144x144.png"
+      />
+    </Apps>
+  );
+};
+
+export const TabsContainer: ComponentStory<typeof Apps> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <Tabs {...args}>
+      <TabButton
+        button={{url: 'www.google.com', title: 'Google', id: 1}}
+        activeId={1}
+      />
+      <TabButton button={{url: 'www.youtube.com', title: 'Youtube', id: 2}} />
+    </Tabs>
+  );
+};
+
+export const ActivityContainer: ComponentStory<typeof Apps> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <Activity {...args}>
+      <>
+        <AppsContainer />
+        <TabsContainer />
+      </>
+    </Activity>
+  );
+};
