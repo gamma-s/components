7c1bb5eab27b1c6208d17996e6b2f561002ab655
Caner Akdas <canerakdas@gmail.com>
2023-01-24 00:20:39 +0300

    refactor: controls container moved to the module css

---
+++ src/components/Container/Controls/index.stories.tsx
@@ -0,0 +1,83 @@
+import {ComponentMeta, ComponentStory} from '@storybook/react';
+import AppButton from '../../Button/App';
+import ControllerButton from '../../Button/Controller';
+import TabButton from '../../Button/Tab';
+
+import {Controls, ControlsLeft, ControlsRight, AppControls} from './';
+
+export default [
+  {
+    title: 'Container/Controls',
+    component: Controls,
+    args: {},
+  } as ComponentMeta<typeof Controls>,
+  {
+    title: 'Container/ControlsLeft',
+    component: ControlsLeft,
+    args: {},
+  } as ComponentMeta<typeof ControlsLeft>,
+  {
+    title: 'Container/ControlsRight',
+    component: ControlsRight,
+    args: {},
+  } as ComponentMeta<typeof ControlsRight>,
+  {
+    title: 'Container/AppControls',
+    component: AppControls,
+    args: {},
+  } as ComponentMeta<typeof AppControls>,
+];
+
+export const ControlsContainer: ComponentStory<typeof Controls> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <Controls {...args}>
+      <ControlsLeftContainer />
+      <ControlsRightContainer />
+    </Controls>
+  );
+};
+
+export const ControlsLeftContainer: ComponentStory<typeof ControlsLeft> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <ControlsLeft {...args}>
+      <ControllerButton
+        buttonDescription="GAMMA"
+        buttonName="HOME"
+        kind="tertiary"
+        wide
+      />
+    </ControlsLeft>
+  );
+};
+
+export const ControlsRightContainer: ComponentStory<typeof ControlsRight> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <ControlsRight {...args}>
+      <ControllerButton buttonDescription="Accept" buttonName="X" />
+      <ControllerButton buttonDescription="Back" buttonName="B" />
+    </ControlsRight>
+  );
+};
+
+export const AppControlsContainer: ComponentStory<typeof AppControls> = ({
+  children,
+  ...args
+}) => {
+  return (
+    <AppControls {...args}>
+      <Controls>
+        <ControlsLeftContainer />
+        <ControlsRightContainer />
+      </Controls>
+    </AppControls>
+  );
+};
---
+++ src/components/Container/Controls/index.tsx
@@ -0,0 +1,133 @@
+/**
+ * @fileoverview This component is helped to see available buttons in the
+ * controller.
+ */
+
+/**
+ * Module dependencies
+ */
+import classNames from 'classnames';
+import {prefix} from '../../../settings/';
+import styles from './index.module.scss';
+
+/**
+ * Controls container
+ * @param {Object} props
+ * @param {React.ReactElement} props.children
+ * @param {string} props.className
+ * @param {Object} props.other
+ * @constructor
+ */
+export const Controls = ({
+  children,
+  className,
+  ...other
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const containerClassPrefix = {
+    [styles[`${prefix}-controller-container`]]: true,
+  };
+
+  const buttonClasses = classNames(className, containerClassPrefix);
+
+  const commonProps = {
+    className: buttonClasses,
+  };
+
+  return (
+    <div className={styles[`${prefix}-controller-wrapper`]}>
+      <div {...other} {...commonProps}>
+        {children}
+      </div>
+    </div>
+  );
+};
+
+/**
+ * Control Left container
+ * @param {Object} props
+ * @param {React.ReactElement} props.children
+ * @param {string} props.className
+ * @param {Object} props.other
+ * @constructor
+ */
+export const ControlsLeft = ({
+  children,
+  className,
+  ...other
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const containerClassPrefix = {
+    [styles[`${prefix}-controller-container--left`]]: true,
+  };
+
+  const buttonClasses = classNames(className, containerClassPrefix);
+
+  const commonProps = {
+    className: buttonClasses,
+  };
+
+  return (
+    <div {...other} {...commonProps}>
+      {children}
+    </div>
+  );
+};
+
+/**
+ * Control Right container
+ * @param {Object} props
+ * @param {React.ReactElement} props.children
+ * @param {string} props.className
+ * @param {Object} props.other
+ * @constructor
+ */
+export const ControlsRight = ({
+  children,
+  className,
+  ...other
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const containerClassPrefix = {
+    [styles[`${prefix}-controller-container--right`]]: true,
+  };
+
+  const buttonClasses = classNames(className, containerClassPrefix);
+
+  const commonProps = {
+    className: buttonClasses,
+  };
+
+  return (
+    <div {...other} {...commonProps}>
+      {children}
+    </div>
+  );
+};
+
+/**
+ * App control container
+ * @param {Object} props
+ * @param {React.ReactElement} props.children
+ * @param {string} props.className
+ * @param {Object} props.other
+ * @constructor
+ */
+export const AppControls = ({
+  children,
+  className,
+  ...other
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  const containerClassPrefix = {
+    [styles[`${prefix}-app-controller-container`]]: true,
+  };
+
+  const buttonClasses = classNames(className, containerClassPrefix);
+
+  const commonProps = {
+    className: buttonClasses,
+  };
+
+  return (
+    <div {...other} {...commonProps}>
+      {children}
+    </div>
+  );
+};
